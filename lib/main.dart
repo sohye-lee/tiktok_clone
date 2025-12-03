34df25e4b6ab6e1030b46d28eeb091381f2bf549
W64 <dev@white64.com>
2025-12-03 05:07:55 -0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFFE9435A),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SignUpScreen(),
+    );
+  }
+}
+
+ 
---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Sign Up'),
+          
+        ],
+      ),
+    );
+  }
+}
